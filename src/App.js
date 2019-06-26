9a1ee9e269f1a8d373039a91a958caaf564c55d6
Justin Navarro <info@justinnavarro.net>
2019-06-26 18:09:32 -0400

    created add todo component, and started to create local state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import TodoList from './todo-list/TodoList';
+import AddTodo from './add-todo/AddTodo';
 import TodoItem from './todo-item/TodoItem';
+import TodoList from './todo-list/TodoList';
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            todos: [],
+        };
+    }
+
     render() {
         return (
             <div className="app">
+                <AddTodo></AddTodo>
                 <TodoList/>
                 <TodoItem/>
             </div>
@@ -17,7 +26,10 @@ class App extends Component {
         const todos = localStorage.getItem('todos');
 
         if (todos) {
-            console.log('has todos');
+            const savedTodos = JSON.parse(todos);
+            this.setState({
+                todos: savedTodos,
+            });
         } else {
             console.log('no todos');
         }
