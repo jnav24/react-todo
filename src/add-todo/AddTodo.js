1e9149817f279563759aaaf26b2e50d185d3ad59
Justin Navarro <info@justinnavarro.net>
2019-06-26 18:15:38 -0400

    create an add todo form

--- src/add-todo/AddTodo.js
+++ src/add-todo/AddTodo.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
 
 export default class AddTodo extends Component {
+    constructor() {
+        super();
+        this.state = {
+            todo: '',
+        };
+    }
+
     render() {
-        return (<div>Add Todo</div>);
+        return (
+            <div className="add-todo">
+                <form onSubmit={(e) => this.submitTodo(e)}>
+                    <input onChange={(e) => this.updateInput(e)} type="text"/>
+                    <button>Add Todo</button>
+                </form>
+            </div>
+        );
+    }
+
+    updateInput = (e) => {
+        this.setState({ todo: e.target.value });
+    }
+
+    submitTodo = (e) => {
+        e.preventDefault();
     }
 }
