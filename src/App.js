a36e876ada56f6e8fb1f1cb15bad3ef0602308f1
Justin Navarro <info@justinnavarro.net>
2019-06-26 18:03:29 -0400

    inject components into app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './todo-list/TodoList';
+import TodoItem from './todo-item/TodoItem';
 
 class App extends Component {
-  render() {
-    return ();
-  }
+    render() {
+        return (
+            <div className="app">
+                <TodoList/>
+                <TodoItem/>
+            </div>
+        );
+    }
+
+    componentDidMount = () => {
+        const todos = localStorage.getItem('todos');
+
+        if (todos) {
+            console.log('has todos');
+        } else {
+            console.log('no todos');
+        }
+    }
 }
 
 export default App;
